83638bed29d70b69594ff977d66b4342704bce3a
martinaruizdev <martinaruizdev@gmail.com>
2024-07-17 21:15:55 -0300

    Changed navbar and modified UI.

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-let togglebtn = document.querySelector(".togglebtn");
-let nav = document.querySelector(".navlinks");
-let links = document.querySelector(".navlinks li");
-
-togglebtn.addEventListener("click", function(){
-    this.classList.toggle("click");
-    nav.classList.toggle("open");
-})
-
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
         if (entry.isIntersecting) {
@@ -15,8 +6,12 @@ const observer = new IntersectionObserver((entries)=>{
     });
 });
 
+
 const hiddenElements = document.querySelectorAll('.hiddenLeft');
 hiddenElements.forEach((el)=> observer.observe(el));
 
 const hiddenElements2 = document.querySelectorAll('.hiddenRight');
 hiddenElements2.forEach((el)=> observer.observe(el));
+
+const hiddenMobile = document.querySelectorAll('.hidden-mobile');
+hiddenMobile.forEach((el)=> observer.observe(el));
